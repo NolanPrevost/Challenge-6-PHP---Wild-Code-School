be4af419977897e189d510fddd5fee2250726ba6
Nolan Prévost <nolan.prevost@gmail.com>
2022-03-28 10:41:34 +0200

    quête sécurisation de formulaire

--- form.php
+++ form.php
@@ -10,19 +10,19 @@
 <form  action="thanks.php"  method="post">
     <div>
       <label  for="nom">Nom :</label>
-      <input  type="text"  id="nom"  name="user_name">
+      <input  type="text"  id="nom"  name="user_lastname" required>
     </div>
     <div>
       <label  for="prenom">Prénom :</label>
-      <input  type="text"  id="prenom"  name="user_firstname">
+      <input  type="text"  id="prenom"  name="user_firstname" required>
     </div>
     <div>
       <label  for="courriel">Courriel :</label>
-        <input  type="email"  id="courriel"  name="user_email">
+        <input  type="email"  id="courriel"  name="user_email" required>
     </div>
     <div>
       <label  for="telephone">Numéro de téléphone :</label>
-      <input  type="text"  id="telephone"  name="user_phone_number">
+      <input  type="text"  id="telephone"  name="user_phone_number" required>
     </div>
     <div>
     <select id="subject" name="subject">
@@ -34,14 +34,11 @@
 </div>
     <div>
       <label  for="message">Message :</label>
-      <textarea  id="message"  name="user_message"></textarea>
+      <textarea  id="message"  name="user_message" required></textarea>
     </div>
     <div  class="button">
       <button  type="submit">Envoyer votre message</button>
     </div>
   </form>
-  <?php
-  var_dump($_GET);
-?>
 </body>
 </html>
--- thanks.php
+++ thanks.php
@@ -1,7 +1,14 @@
 <?php
-echo "Merci " . $_POST["user_firstname"] . " " . $_POST["user_name"] . " de nous avoir contacté à propos de " . $_POST["subject"] . ". " . PHP_EOL;
 
-echo "Un de nos conseiller vous contactera soit à l’adresse " . $_POST["user_email"] . " ou par téléphone au " . $_POST["user_phone_number"] . " dans les plus brefs délais pour traiter votre demande : " . PHP_EOL; 
+$message = "Attention, il faut bien remplir tous les champs !";
 
-echo $_POST["user_message"];
-  ?>
+if (filter_var($_POST["user_email"], FILTER_VALIDATE_EMAIL)) {
+  if ($_POST["user_firstname"] && $_POST["user_phone_number"] && $_POST["user_message"]) {
+    echo "Merci " . $_POST["user_firstname"] . " " . " de nous avoir contacté à propos de " . $_POST["subject"] . ". " . "Un de nos conseiller vous contactera soit à l’adresse " . $_POST["user_email"] . " ou par téléphone au " . $_POST["user_phone_number"] . " dans les plus brefs délais pour traiter votre demande : " . $_POST["user_message"];
+  } else {
+    echo $message;
+  }
+} else {
+  echo "Adresse mail non valide";
+}
+?>
